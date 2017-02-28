3ba1c311bd716539c209fe7a303e4df22133ff46
= <jefftam23@gmail.com>
2017-02-28 11:26:04 -0500

    Finish has_one_through implementation

--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -5,15 +5,17 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
+    through_options = assoc_options[through_name]
 
     define_method(name) do
-      through_options = assoc_options[through_name]
+      through_obj_instance = self.send(through_name)
+
       source_options = through_options.model_class.assoc_options[source_name]
 
       through_table_name = through_options.model_class.table_name
       source_table_name = source_options.model_class.table_name
 
-      source_arr = DBConnection.execute(<<-SQL, THROUGH_INSTANCE_ID_GOES_HERE)
+      source_arr = DBConnection.execute(<<-SQL, through_obj_instance.id)
         SELECT
           "#{source_table_name}".*
         FROM
@@ -21,7 +23,7 @@ module Associatable
         JOIN
           #{through_table_name}
         ON
-          #{through_table_name}.#{through_options.foreign_key} = #{source_table_name}.id
+          #{through_table_name}.#{source_options.foreign_key} = #{source_table_name}.id
         WHERE
           #{through_table_name}.id = ?
       SQL
